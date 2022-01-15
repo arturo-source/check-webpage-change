4cd6a2d7937fe1b2cd325ae5fb146f0df3ebe7f8
arturo-source <arturojuan9898@gmail.com>
2022-01-15 12:48:54 +0100

    Added json config file for facilities

--- check-change.php
+++ check-change.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 
 // function from https://stackoverflow.com/a/32875341
-function sendMessage($messaggio) {
-    $chatID = "";
-    $token = "";
+function sendMessage($messaggio)
+{
+    global $setup;
 
-    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
+    $url = "https://api.telegram.org/bot" . $setup["bot_token"] . "/sendMessage?chat_id=" . $setup["chat_id"];
     $url = $url . "&text=" . urlencode($messaggio);
     $ch = curl_init();
     $optArray = array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true
     );
     curl_setopt_array($ch, $optArray);
     $result = curl_exec($ch);
@@ -18,18 +18,32 @@ function sendMessage($messaggio) {
     return $result;
 }
 
-$url = "";
-$xpath_nodes = [];
+function ReadJson($setup_filename)
+{
+    $json_content_str = file_get_contents($setup_filename);
+    $setup = json_decode($json_content_str, true);
 
-$html = file_get_contents($url);
-$doc  =	new DOMDocument();
+    if (
+        $setup === null
+        && json_last_error() !== JSON_ERROR_NONE
+    ) {
+        exit("$setup_filename is malformed.");
+    }
+
+    return $setup;
+}
+
+$setup = ReadJson("");
+
+$html = file_get_contents($setup["url"]);
+$doc  =    new DOMDocument();
 @$doc->loadHTML($html);
 $xpath = new DOMXpath($doc);
 
 $msg = "";
-foreach ($xpath_nodes as $xpath_node) {
+foreach ($setup["xpaths"] as $key => $xpath_node) {
     $node = $xpath->query($xpath_node);
-    $msg .= $node[0]->nodeValue . "\n";
+    $msg .= "$key: {$node[0]->nodeValue} \n";
 }
 
 sendMessage($msg);
